cd418d3ebb25c0f6d671a7c56aa5cd0f5809ab8d
Stuart Macleod <smacleod@akamai.com>
2024-04-26 14:58:22 -0700

    Adding AK variables

--- script.py
+++ script.py
@@ -1,4 +1,20 @@
 import os
+import argparse
 
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
+# Set options
+usage = "usage: python get_security_object_usage.py --name MyCustomRule [--edgerc ~/.edgerc] [--section default] [--accountSwitchKey <ask>] [--debug]"
+parser = argparse.ArgumentParser(usage=usage)
+parser.add_argument(
+    "--akamai_client_token", action="store", dest="akamai_client_token", required=True, help="Akamai Client Token"
+)
+parser.add_argument(
+    "--akamai_access_token", action="store", dest="akamai_access_token", required=True, help="Akamai Access Token"
+)
+parser.add_argument(
+    "--akamai_client_secret", action="store", dest="akamai_client_secret", required=True, help="Akamai Client Secret"
+)
+parser.add_argument("--akamai_host", action="store", dest="akamai_host", required=True, help="Akamai Host")
+
+args = parser.parse_args()
+
+print(str(args))
